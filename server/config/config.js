44efac9fb1fec845b067fc231f2fd3ddf83b289d
jidemobell <jidd32@gmail.com>
2019-04-02 11:30:31 +0100

    delete folders (to be moved to feature)

--- server/config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 8081,
-  database: process.env.MONGODB_URI || 'mongod://localhost/bears15',
-  jwtPrivateKey: process.env.JWT_PRIVATE || 'prvate super secret key'
-}
