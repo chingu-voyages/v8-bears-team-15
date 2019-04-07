d82b7b45c7378723ad3b066f17f2a3de4a6df5b4
jidemobell <jidd32@gmail.com>
2019-04-07 19:50:42 +0100

    add store to session middleware

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 import express from 'express';
+import session from 'express-session';
+import passport from 'passport';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import morgan from 'morgan';
-import colors from 'colors';
+import store from 'connect-mongo';
+
+
+
 import { config } from './config/config';
 import { logger } from './helper/logger';
 
 // const config = require('./config/config');
 
+const MongoStore = store(session);
 const app = express();
 
 mongoose.connect(config.database, { useNewUrlParser: true })
@@ -21,11 +27,25 @@ mongoose.connect(config.database, { useNewUrlParser: true })
     });
   });
 
+
+app.use(session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection  
+    }),
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  }));
+
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
+app.use(passport.initialize());
+app.use(passport.session());
 // require('./routes')(app)
+
 
 app.listen(config.port, () => {
   logger.info(`CORS-enabled web server listening on port ${config.port}`);
