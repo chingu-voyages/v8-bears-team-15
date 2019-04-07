88e2b72c93a7674099ccc2ac7b117b01cba175c8
jidemobell <jidd32@gmail.com>
2019-04-07 19:08:13 +0100

    update config with dotenv and facebook app details

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,15 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const config = {
   port: process.env.PORT || 8081,
   database: process.env.MONGODB_URI || 'mongodb://localhost/bears04',
   jwtPrivateKey: process.env.JWT_PRIVATE || 'private super secret key'
 }
 
-// export { config }
+export const facebook = {
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: `http://127.0.0.1:${config.port}/server/auth/facebook/callback`
+}
