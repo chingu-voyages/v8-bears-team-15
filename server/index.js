1dcdaab97b945b52e516353a1a103726946a494e
jidemobell <jidd32@gmail.com>
2019-04-07 19:21:45 +0100

    import logger to index

--- server/index.js
+++ server/index.js
@@ -4,16 +4,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import colors from 'colors';
 import { config } from './config/config';
-
-const logger = require('tracer').colorConsole({
-  filters: [
-    colors.underline, colors.green,
-    {
-      warn: colors.yellow,
-      error: [colors.red, colors.bold],
-    },
-  ],
-});
+import { logger } from './helper/logger';
 
 // const config = require('./config/config');
 
