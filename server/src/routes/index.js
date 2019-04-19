e92edcecb9ae7d821e8981798e4a7aff4af5cf63
jidemobell <jidd32@gmail.com>
2019-04-19 09:47:28 +0100

    add scope to google route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -9,55 +9,54 @@ const router = express.Router();
 
 // Messages
 router.get('/', (req, res) => {
-  res.status(200).send({ 
+  res.status(200).send({
     message: 'Welcome to Jobbatical clone application API'
   });
 });
 
 // Default
 // router.get('/*', (req, res) => {
-//   res.status(200).send({ 
-//     message: 'The endpoint you have hit does not exist' 
+//   res.status(200).send({
+//     message: 'The endpoint you have hit does not exist'
 //   });
 // });
 
 
 // GOOGLE - may require scope definition
 
-router.get('/login/google',  passportGoogle.authenticate('google'));
+router.get('/login/google', passportGoogle.authenticate('google',
+  {
+    scope: ['https://www.googleapis.com/auth/userinfo.email']
+  }));
 
-router.get('/login/google/callback', 
-passportGoogle.authenticate('google',{
-    failureRedirect: '/login',
-    successRedirect: '/home'
-  })
-  )
+router.get('/login/google/callback',
+  passportGoogle.authenticate(
+    'google', {
+      failureRedirect: '/login',
+      successRedirect: '/home'
+    }
+  ));
+
 
 // FACEBOOK - may require scope definition
 
-router.get('/login/facebook',  passportFacebook.authenticate('facebook'));
+router.get('/login/facebook', passportFacebook.authenticate('facebook'));
 
-router.get('/login/facebook/callback', 
-passportFacebook.authenticate('facebook',{
+router.get('/login/facebook/callback',
+  passportFacebook.authenticate('facebook', {
     failureRedirect: '/login',
     successRedirect: '/home'
-  })
-  )
-
-
-
- 
+  }));
 
 // LINKEDIN - may require scope definition
 
-router.get('/login/linkedin',  passportLinkedin.authenticate('linkedin'));
+router.get('/login/linkedin', passportLinkedin.authenticate('linkedin'));
 
-router.get('/login/linkedin/callback', 
-passportLinkedin.authenticate('linkedin',{
+router.get('/login/linkedin/callback',
+  passportLinkedin.authenticate('linkedin', {
     failureRedirect: '/login',
     successRedirect: '/home'
-  })
-  )
+  }));
 
 
 // router.get('/login', signIn)
