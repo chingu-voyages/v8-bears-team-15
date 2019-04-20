6ea5135e67bc6992cd391d86df6f8b66a16ed938
jidemobell <jidd32@gmail.com>
2019-04-20 13:22:49 +0100

    add more user actions

--- client/src/actions/actionConstants.js
+++ client/src/actions/actionConstants.js
@@ -1,3 +1,8 @@
 //constants associated with Home state
 export const PLAY_INTRO = 'PLAY_INTRO';
 export const POP_WINDOW = 'POP_WINDOW';
+
+//user action types
+export const SIGN_IN = 'SIGN_IN';
+export const USER_DASHBOARD = 'USER_DASHBOARD';
+export const USER_ERROR ='USER_ERROR';
