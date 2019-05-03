6761261ce1489db4b6477c7857bf82be080e6ccc
jidemobell <jidd32@gmail.com>
2019-05-03 11:40:16 +0100

    add code to script

--- client/src/components/Cards/Listings/ListingCard.js
+++ client/src/components/Cards/Listings/ListingCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import testpic from '../../../images/testcard.jpg'
+// import testpic from '../../../images/testcard.jpg'
 import './ListingCard.css';
 
 function ListingCard(props) {
