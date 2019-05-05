9d00ec37d7907018466fd882399d936355f7bb54
jidemobell <jidd32@gmail.com>
2019-05-05 12:36:05 +0100

    Updates

---
+++ precache-manifest.c231f65a609bb7c165cc0af8bb71f174.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "80b5b8188c77c1e0697bfafcd889eea1",
+    "url": "/static/media/jobbatical_logo_blue.80b5b818.svg"
+  },
+  {
+    "revision": "c448fca962db5f20d5fd790dbc821423",
+    "url": "/static/media/jb_logo_white.c448fca9.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c5fe0d46d8f4c0e5bfc0",
+    "url": "/static/js/main.b95f833f.chunk.js"
+  },
+  {
+    "revision": "2ce24935faff9d01d305",
+    "url": "/static/js/2.0748980a.chunk.js"
+  },
+  {
+    "revision": "c5fe0d46d8f4c0e5bfc0",
+    "url": "/static/css/main.de1046b2.chunk.css"
+  },
+  {
+    "revision": "2ce24935faff9d01d305",
+    "url": "/static/css/2.8eabcbc3.chunk.css"
+  },
+  {
+    "revision": "cbf2ceb87c32405f58a41b86888133af",
+    "url": "/index.html"
+  }
+];
