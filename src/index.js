c2de92fbdd62e99592e436b3b1dd3d510d079130
jidemobell <jidd32@gmail.com>
2019-05-05 13:48:56 +0100

    update index  with serve

--- src/index.js
+++ src/index.js
@@ -57,10 +57,10 @@ app.use('/', routes);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  Server.use(express.static(path.join(__dirname, 'client/build')));
+  Server.use(express.static(path.join(__dirname, '../client/build')));
   // Handle React routing, return all requests to React app
   Server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
